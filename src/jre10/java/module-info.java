7bbd30fcd3318a54e9f4342c88e42dc8608706aa
GedMarc <ged_marc@hotmail.com>
2018-07-23 19:34:29 +0200

    JDK 10 Service Loading rather than Classpath Search (Much Faster), Build update to classify JDK 10 dependencies properly

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,12 @@
+import com.jwebmp.core.base.angular.services.IAngularDirective;
+import com.jwebmp.plugins.angularautoexpand.AngularAutoExpandDirective;
+
 module com.jwebmp.plugins.angularautoexpand {
 	exports com.jwebmp.plugins.angularautoexpand;
 
 	requires com.jwebmp.core;
+
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularautoexpand.AngularAutoExpandPageConfigurator;
+	provides IAngularDirective with AngularAutoExpandDirective;
+	
 }
