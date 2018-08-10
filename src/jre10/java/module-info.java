af34870f90e4c2bb91734e2d3dca2c180644f1d1
GedMarc <ged_marc@hotmail.com>
2018-08-10 13:29:59 +0200

    Plugin POM updates for JRE10, and maven dependency classification bug fix

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -5,8 +5,9 @@ module com.jwebmp.plugins.angularautoexpand {
 	exports com.jwebmp.plugins.angularautoexpand;
 
 	requires com.jwebmp.core;
+	requires java.validation;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularautoexpand.AngularAutoExpandPageConfigurator;
 	provides IAngularDirective with AngularAutoExpandDirective;
-	
+
 }
