a0ec89a58712f41c2616e45cd3e3984ccee73d42
GedMarc <ged_marc@hotmail.com>
2018-10-31 12:53:56 +0200

    Remove Serializable by default, Update module-info to utilize transitive dependencies

--- src/main/java/com/jwebmp/plugins/angularautoexpand/AngularAutoExpandDirective.java
+++ src/main/java/com/jwebmp/plugins/angularautoexpand/AngularAutoExpandDirective.java
@@ -15,7 +15,6 @@ public class AngularAutoExpandDirective
 		implements IAngularDirective<AngularAutoExpandDirective>
 {
 
-	private static final long serialVersionUID = 1L;
 
 	/*
 	 * Constructs a new AngularAutoExpandDirective
